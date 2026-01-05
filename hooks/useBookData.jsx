e9976e264aa6d72f9a0a6df9a6f2cff0a497cdc9
Agoth Arop <agoth.a3@gmail.com>
2026-01-04 17:53:29 -0700

    Implemented basic cache with localstorage
    - Also centered the loading message when fetching

--- hooks/useBookData.jsx
+++ hooks/useBookData.jsx
@@ -5,20 +5,29 @@ const useBookData = () => {
 	const [error, setError] = useState(null);
 	const [loading, setIsLoading] = useState(true);
 
+	const cacheKey = "books:economics:v1";
+	const url =
+		"https://www.googleapis.com/books/v1/volumes?q=subject:economics&maxResults=40";
+
 	useEffect(() => {
-		fetch(
-			"https://www.googleapis.com/books/v1/volumes?q=subject:economics&maxResults=40",
-			{
-				"Access-Control": "Allow-Origin",
-			}
-		)
+		const cached = localStorage.getItem(cacheKey);
+		if (cached) {
+			setProducts(JSON.parse(cached));
+			setIsLoading(false);
+			return;
+		}
+		fetch(url)
 			.then((response) => {
 				if (response.status >= 400) {
 					throw new Error("Server error");
 				}
 				return response.json();
 			})
-			.then((prods) => setProducts(prods.items))
+			.then((prods) => {
+                const items = prods.items || []
+				setProducts(items);
+                localStorage.setItem(cacheKey, JSON.stringify(items))
+			})
 			.catch((error) => setError(error))
 			.finally(() => setIsLoading(false));
 	}, []);
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,6 +1,6 @@
 const Loading = () => {
   return (
-    <section>
+    <section className="flex flex-1 items-center justify-center">
         <h1 className="text-4xl">Loading...</h1>
     </section>
   )
