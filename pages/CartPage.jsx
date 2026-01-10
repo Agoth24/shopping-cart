7276e4d2305d7d02f4d7308d71a2215397350e2a
Agoth Arop <agoth.a3@gmail.com>
2026-01-09 22:06:15 -0700

    centered cart page placeholder text

--- pages/CartPage.jsx
+++ pages/CartPage.jsx
@@ -1,6 +1,6 @@
 const CartPage = () => {
-    return (
-        <section className="p-8">
+	return (
+		<section className="p-8 flex items-center justify-center">
 			<h1 className="text-3xl font-bold">Cart Page</h1>
 		</section>
 	);
