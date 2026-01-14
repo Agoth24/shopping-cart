571a52fa6c12ec7cac8a43b1af84a6e0b790fef1
Agoth Arop <agoth.a3@gmail.com>
2026-01-13 21:03:13 -0700

    audited npm issues + spinner import

--- pages/ShopPage.jsx
+++ pages/ShopPage.jsx
@@ -2,6 +2,8 @@ import BookCard from "../src/components/BookCard";
 import books from "../assets/books";
 
 const ShopPage = () => {
+
+    
 	return (
 		<section className="p-4 md:p-8">
 			<div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-6 rounded-xl">
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,8 +1,8 @@
+import { BarLoader } from "react-spinners"
+
 const Loading = () => {
   return (
-    <section className="flex flex-1 items-center justify-center">
-        <h1 className="text-4xl">Loading...</h1>
-    </section>
+    <BarLoader />
   )
 }
 
