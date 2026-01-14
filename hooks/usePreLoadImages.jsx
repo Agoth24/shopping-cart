1537bc3b61dfc1be75117e58190bbb214f8db08e
Agoth Arop <agoth.a3@gmail.com>
2026-01-13 21:18:46 -0700

    add usePreLoadImages hook and update ShopPage structure

---
+++ hooks/usePreLoadImages.jsx
@@ -0,0 +1,9 @@
+import { useEffect } from "react"
+
+const usePreLoadImages = () => {
+    useEffect(() => {
+
+    })
+}
+
+export default usePreLoadImages
--- pages/ShopPage.jsx
+++ pages/ShopPage.jsx
@@ -4,16 +4,16 @@ import Loading from "../src/components/Loading";
 
 const ShopPage = () => {
 
-    return <Loading />
-	// return (
-	// 	<section className="p-4 md:p-8">
-	// 		<div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-6 rounded-xl">
-	// 			{books.map((book) => (
-	// 				<BookCard key={book.key} product={book} />
-	// 			))}
-	// 		</div>
-	// 	</section>
-	// );
+    // return <Loading />
+	return (
+		<section className="p-4 md:p-8">
+			<div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-6 rounded-xl">
+				{books.map((book) => (
+					<BookCard key={book.key} product={book} />
+				))}
+			</div>
+		</section>
+	);
 };
 
 export default ShopPage;
