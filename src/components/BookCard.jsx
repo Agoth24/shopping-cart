1cadaf023bac73ff17374163a9223b0f11820b6b
Agoth Arop <agoth.a3@gmail.com>
2026-01-17 21:27:54 -0700

    added add buttons to book cards

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,16 +1,16 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import DefaultCover from "./DefaultCover";
-
+import { CirclePlus, Check } from "lucide-react";
 const BookCard = ({ product, coverUrl }) => {
+	const [added, setAdded] = useState(false);
+
 	const author = Array.isArray(product.author_name)
 		? product.author_name[0]
 		: product.author_name;
 	return (
 		// Card component with image, title, author and rating
-		<Link
-			to="/"
-			className="p-4 hover:bg-zinc-200 dark:hover:bg-zinc-900 rounded-xl ease-in-out duration-100"
-		>
+		<div className="p-4 hover:bg-zinc-200 dark:hover:bg-zinc-900 rounded-xl ease-in-out duration-100">
 			<div key={product.key} className="flex flex-col justify-between">
 				<div className="aspect-2/3 w-full mb-2 overflow-hidden">
 					{coverUrl ? (
@@ -25,10 +25,22 @@ const BookCard = ({ product, coverUrl }) => {
 				</div>
 				<div className="flex flex-col gap-2 justify-self-end">
 					<h3 className="text-sm text-gray-500">{author}</h3>
-					<h1 className="text-xl font-bold">{product.title}</h1>
+					<div className="flex gap-4 justify-between items-center">
+						<h1 className="text-xl font-bold">{product.title}</h1>
+						<button
+							onClick={() => setAdded(!added)}
+							className="cursor-pointer hover:text-green-500"
+						>
+							{!added ? (
+								<CirclePlus className="shrink-0" size={25} />
+							) : (
+								<Check className="shrink-0" size={25} />
+							)}
+						</button>
+					</div>
 				</div>
 			</div>
-		</Link>
+		</div>
 	);
 };
 
